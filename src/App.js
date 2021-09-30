503f151b88cf66c5de263ae1957887e4a81c99e3
Suhair Aljammal <saljammal995@gmail.com>
2021-09-30 13:47:38 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import links from './data/links.json'
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [links, setLinks] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Suha995/Ex-Deploy-Suha/main/src/data/links.json"
+      );
+      const data = await response.json();
+      setLinks(data);
+    })();
+  }, []);
   return (
     <div className="App">
       <h2>Links</h2>
       <ul>
-      {links.map((link, index) => {
-        return(
-          <li><a href={link.url} target='_blank' rel='noreferrer'>{link.title}</a></li>
-        )
-      })}
+        {links.map((link, index) => {
+          return (
+            <li>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.title}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
